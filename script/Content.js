0535e57836ddf8bea7bc1f51694f120b658b73b9
Mali <lindatrana06@gmail.com>
2024-03-25 06:38:39 -0600

    Organizando el proyecto. Aniadi 2 carpetas: script y textos.

---
+++ script/Content.js
@@ -0,0 +1,6 @@
+console.log('Hola content')
+
+let frutas = ['Arandanos','Fresa','Ciruela','Manzana']
+
+let nueva = frutas.splice(1)
+console.log(nueva)
---
+++ script/nuevo.js
@@ -0,0 +1,25 @@
+verDatos = () => {
+  hola = (sla) => {
+    sla='nuevo dato ingresado'
+    var nueva;
+  }
+}
+
+class Nombre {
+  constructor(nombre,apellido){
+    this.nombre=nombre,
+    this.apellido=apellido
+  }
+}
+
+let nuevoNombre = new Nombre()
+
+frutas = [
+  {
+    Nombre:'Uva',
+    especie:{av:'rut'},
+    categoria: 'Sin semilla',
+    caracteristicas:['con semilla','Morado','redonda']
+  }
+]
+
